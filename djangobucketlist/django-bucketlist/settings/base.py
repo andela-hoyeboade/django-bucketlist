5d42f3d6cd406f50bbc424db879de451ecc4c384
andela-hoyeboade <hassan.oyeboade@andela.com>
2016-07-22 10:18:23 +0100

    [Chore #126460837] Modify base settings

--- djangobucketlist/django-bucketlist/settings/base.py
+++ djangobucketlist/django-bucketlist/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5tj5%t52c0+eyjhb#5rvcx**kw900---tuj+9-q7#6fd$(@lb-'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
