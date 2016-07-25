658d0484325e362c2cc57337d4424f3269075eeb
andela-hoyeboade <hassan.oyeboade@andela.com>
2016-07-25 12:12:06 +0100

    [Fix #126460445] Make changes in settings

--- djangobucketlist/django-bucketlist/settings/__init__.py
+++ djangobucketlist/django-bucketlist/settings/__init__.py
@@ -9,6 +9,4 @@ if os.getenv('CI') and not os.getenv('HEROKU'):
     from .development import *
 
 if os.getenv('HEROKU') is not None:
-    from django_envie.workroom import convertfiletovars
-    convertfiletovars()
     from .production import *
