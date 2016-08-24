994e81500244cfdc0b4beadf76a391de8615e75b
andela-hoyeboade <hassan.oyeboade@andela.com>
2016-08-24 16:46:31 +0100

    [Chore #128967537] Add more comments to code

--- djangobucketlist/bucketlist/admin.py
+++ djangobucketlist/bucketlist/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 
 from .models import BucketList, BucketListItem
 
-# Register your models here.
-
 admin.site.register(BucketList)
 admin.site.register(BucketListItem)
